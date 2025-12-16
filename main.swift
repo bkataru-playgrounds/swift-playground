70dd8847a7e1c9bbfb3db11d2bd901d4d81297ff
bkataru <baalateja.k@gmail.com>
2025-12-17 03:03:03 +0530

    first commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+var points = 10
+points += 7
+
+let title: String? = "Rookie" // change to nil and re-run
+
+print("points =", points)
+
+if points >= 15 {
+	print("LEVEL UP")
+} else {
+	print("KEEP GOING")
+}
